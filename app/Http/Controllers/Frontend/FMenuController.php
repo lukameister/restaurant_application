852eb5be2abd6e4f0e2bec5bb3cd6de983044d57
lukameister <lukaxman2@gmail.com>
2023-04-04 18:53:54 +0200

    Displaying menus to the /menus page

--- app/Http/Controllers/Frontend/FMenuController.php
+++ app/Http/Controllers/Frontend/FMenuController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Menu;
 use Illuminate\Http\Request;
 
@@ -11,7 +12,8 @@ class FMenuController extends Controller
     public function index()
     {
         $menus = Menu::all();
+        $categories = Category::all();
 
-        return view('menus.index', compact('menus'));
+        return view('menus.index', compact('menus', 'categories'));
     }
 }
